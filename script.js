c1342ff8402b09297f26fd41c0486d5d2d810112
aadildil <117893260+aadildil@users.noreply.github.com>
2023-08-04 20:01:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
 // your code here
+
+	let a=0;
+	let b=1;
+	let c=a;
+	if(num===1)
+		return 0;
+	else if(num===2)
+		return 1;
+	for(let i=2;i<num;i++)
+		{
+			c=b;
+			b=b+a;
+			a=c;
+		}
+	return b;
+	
+		
 }
 
 module.exports = fibonacci;
